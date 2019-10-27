f208cc83a816871908f41ee533aa70c6715b4dfe
Sam <sam@samtracey.co.uk>
2019-10-27 21:17:13 +0000

    experiments with borders

--- src/IrregularGrid.elm
+++ src/IrregularGrid.elm
@@ -6,11 +6,12 @@ import Element.Background as Background
 import Element.Border as Border
 import Element.Font as Font
 import Html.Attributes exposing (style)
+import Maybe.Extra as Maybe
 import Set exposing (Set)
 
 
 baseSize =
-    30
+    50
 
 
 smallSquare =
@@ -37,6 +38,25 @@ cross =
         |> List.map (\item -> { coords = ( item.x, item.y ), content = item.content })
 
 
+pt =
+    ChemElements.elementData
+        |> List.map mapChemElements
+        |> List.filter (\e -> e.content /= "")
+
+
+mapChemElements e =
+    case e.group of
+        Group n ->
+            -- if n > 2 then
+            --     { coords = ( n - 10, e.period ), content = e.symbol }
+            --
+            -- else
+            { coords = ( n, e.period ), content = e.symbol }
+
+        _ ->
+            { coords = ( 0, 0 ), content = "" }
+
+
 view : Element msg
 view =
     cross
@@ -48,28 +68,123 @@ toGrid data =
     -- TODO sort =
     let
         allX =
-            List.range 1 3
+            List.range 1 (Maybe.withDefault 1 (List.maximum (List.map (\a -> Tuple.first a.coords) data)))
 
         allY =
-            List.range 1 3
+            List.range 1 (Maybe.withDefault 1 (List.maximum (List.map (\a -> Tuple.second a.coords) data)))
 
         styleOrInsertMissingCells y x =
+            let
+                cellPresent =
+                    List.filter (\item -> item.coords == ( x, y )) data
+                        |> List.head
+                        |> Maybe.isJust
+
+                hasRightNeighbour =
+                    List.filter (\item -> item.coords == ( x + 1, y )) data
+                        |> List.head
+                        |> Maybe.isJust
+
+                hasBelowNeighbour =
+                    List.filter (\item -> item.coords == ( x, y + 1 )) data
+                        |> List.head
+                        |> Maybe.isJust
+
+                styles =
+                    { leftBorder =
+                        if cellPresent then
+                            1
+
+                        else
+                            0
+                    , topBorder =
+                        if cellPresent then
+                            1
+
+                        else
+                            0
+                    , rightBorder =
+                        if cellPresent && not hasRightNeighbour then
+                            1
+
+                        else
+                            0
+                    , bottomBorder =
+                        if cellPresent && not hasBelowNeighbour then
+                            1
+
+                        else
+                            0
+                    , backgroundColor =
+                        if cellPresent then
+                            rgb255 100 200 100
+
+                        else
+                            rgba 0 0 0 0
+                    , cellWidth =
+                        if cellPresent && not hasRightNeighbour then
+                            px (round (baseSize + 1))
+
+                        else
+                            px baseSize
+                    , cellHeight =
+                        if cellPresent && not hasBelowNeighbour then
+                            px (round (baseSize + 1))
+
+                        else if cellPresent && hasBelowNeighbour then
+                            px (round (baseSize + 1))
+
+                        else
+                            px (round baseSize)
+
+                    -- , paddingTop =
+                    --     if cellPresent then
+                    --         1
+                    --
+                    --     else
+                    --         0
+                    -- , paddingRight =
+                    --     if cellPresent && not hasRightNeighbour then
+                    --         1
+                    --
+                    --     else
+                    --         0
+                    -- , paddingBottom =
+                    --     if cellPresent && not hasBelowNeighbour then
+                    --         1
+                    --
+                    --     else
+                    --         0
+                    -- , paddingLeft =
+                    --     if cellPresent then
+                    --         1
+                    --
+                    --     else
+                    --         0
+                    }
+
+                cellStyles =
+                    [ width styles.cellWidth
+                    , height styles.cellHeight
+                    , padding 0 --Each { top = styles.paddingTop, right = styles.paddingRight, bottom = styles.paddingBottom, left = styles.paddingLeft }
+                    , Border.widthEach { bottom = styles.bottomBorder, left = styles.leftBorder, right = styles.rightBorder, top = styles.topBorder }
+                    , Background.color styles.backgroundColor
+                    ]
+            in
             List.map .content (List.filter (\item -> item.coords == ( x, y )) data)
                 |> List.head
                 |> Maybe.withDefault ""
                 |> text
-                |> el
-                    [ width (px 30)
-                    , height (px 30)
-                    , Border.widthEach { bottom = 0, left = 1, right = 0, top = 1 }
-                    ]
+                |> el [ centerX, centerY ]
+                |> el cellStyles
 
         toRow n =
             List.map (styleOrInsertMissingCells n) allX
-                |> row []
+                |> row
+                    []
     in
-    column [ padding 20 ]
-        (List.map (\x -> toRow x) allY)
+    column [ padding 10 ]
+        (List.map toRow allY)
 
 
 
